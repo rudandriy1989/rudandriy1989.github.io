eb439a9a26152d80ada7b20d80a8d664515c6279
AndriyRud <a.rudik@i.ua>
2017-09-21 10:21:09 +0300

    new

---
+++ js/sticky1.js
@@ -0,0 +1,7 @@
+$(window).scroll(function() {
+        if ($(this).scrollTop() > 1){  
+        $('nav').addClass("sticky");
+        }
+        else{
+        $('nav').removeClass("sticky");
+        }});
